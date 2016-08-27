d238919943ad434e170926bb670f5995b0a14ed8
Vivek Dua <vivekdua81@gmail.com>
2016-08-27 14:09:02 +0530

    Adding console project

---
+++ ConsoleSandbag/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleSandbag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vikisha here!");
+            Console.ReadLine();
+        }
+    }
+}
